0b17ceb093f09105af2f55ceb4d45b129b77d9b0
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2013-10-04 06:35:35 +0000

    New project USB vendor specific class test

---
+++ trunk/usb-vendor-specific/src/main.c
@@ -0,0 +1,63 @@
+#include <stm32f4xx.h>
+#include <stdio.h>
+
+#include <usb_core.h> // OTG driver
+#include <usbd_core.h> // Device library
+#include <usbd_usr.h> // USBD_Usr_cb_TypeDef structure implementaion
+#include <usbd_hid_core.h> // Class library
+#include "usbd_desc.h"
+
+__ALIGN_BEGIN USB_OTG_CORE_HANDLE  USB_OTG_dev __ALIGN_END;
+
+/**
+ * For printf.
+ */
+void initUsart (void)
+{
+        RCC_APB2PeriphClockCmd (RCC_APB2Periph_USART1, ENABLE);
+        RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+        GPIO_InitTypeDef gpioInitStruct;
+
+        gpioInitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+        gpioInitStruct.GPIO_Mode = GPIO_Mode_AF;
+        gpioInitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+        gpioInitStruct.GPIO_OType = GPIO_OType_PP;
+        gpioInitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+        GPIO_Init(GPIOB, &gpioInitStruct);
+        GPIO_PinAFConfig (GPIOB, GPIO_PinSource6, GPIO_AF_USART1); // TX
+        GPIO_PinAFConfig (GPIOB, GPIO_PinSource7, GPIO_AF_USART1); // RX
+
+        USART_InitTypeDef usartInitStruct;
+        usartInitStruct.USART_BaudRate = 9600;
+        usartInitStruct.USART_WordLength = USART_WordLength_8b;
+        usartInitStruct.USART_StopBits = USART_StopBits_1;
+        usartInitStruct.USART_Parity = USART_Parity_No;
+        usartInitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+        usartInitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+        USART_Init (USART1, &usartInitStruct);
+        USART_Cmd (USART1, ENABLE);
+}
+
+/**
+ * Configure the USB OTG engine. This will be a HID class device.
+ */
+void initUsb (void)
+{
+        USBD_Init(&USB_OTG_dev,
+                  USB_OTG_FS_CORE_ID,
+                  &USR_desc,
+                  &USBD_HID_cb,
+                  &USR_cb);
+}
+
+int main (void)
+{
+        initUsart ();
+        printf ("main function started\r\n");
+        initUsb ();
+
+        while (1) {
+
+        }
+}
---
+++ trunk/usb-vendor-specific/src/stm32fxxx_it.c
@@ -0,0 +1,224 @@
+/**
+  ******************************************************************************
+  * @file    stm32fxxx_it.c
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    19-March-2012
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides all exceptions handler and peripherals interrupt
+  *          service routine.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32fxxx_it.h"
+#include "usb_core.h"
+#include "usbd_core.h"
+#include "usbd_hid_core.h"
+#include "usb_conf.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+
+#define CURSOR_STEP     10
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+__IO uint32_t remote_wakeup =0;
+/* Private function prototypes -----------------------------------------------*/
+extern USB_OTG_CORE_HANDLE           USB_OTG_dev;
+static uint8_t *USBD_HID_GetPos (void);
+extern uint32_t USBD_OTG_ISR_Handler (USB_OTG_CORE_HANDLE *pdev);
+#ifdef USB_OTG_HS_DEDICATED_EP1_ENABLED 
+extern uint32_t USBD_OTG_EP1IN_ISR_Handler (USB_OTG_CORE_HANDLE *pdev);
+extern uint32_t USBD_OTG_EP1OUT_ISR_Handler (USB_OTG_CORE_HANDLE *pdev);
+#endif
+
+/******************************************************************************/
+/*             Cortex-M Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+* @brief   This function handles NMI exception.
+* @param  None
+* @retval None
+*/
+void NMI_Handler(void)
+{
+}
+
+/**
+* @brief  This function handles Hard Fault exception.
+* @param  None
+* @retval None
+*/
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+* @brief  This function handles Memory Manage exception.
+* @param  None
+* @retval None
+*/
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+* @brief  This function handles Bus Fault exception.
+* @param  None
+* @retval None
+*/
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+* @brief  This function handles Usage Fault exception.
+* @param  None
+* @retval None
+*/
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+* @brief  This function handles SVCall exception.
+* @param  None
+* @retval None
+*/
+void SVC_Handler(void)
+{
+}
+
+/**
+* @brief  This function handles Debug Monitor exception.
+* @param  None
+* @retval None
+*/
+void DebugMon_Handler(void)
+{
+}
+
+/**
+* @brief  This function handles PendSVC exception.
+* @param  None
+* @retval None
+*/
+void PendSV_Handler(void)
+{
+}
+
+/**
+* @brief  This function handles SysTick Handler.
+* @param  None
+* @retval None
+*/
+void SysTick_Handler(void)
+{
+  
+  uint8_t *buf;
+  
+  buf = USBD_HID_GetPos();
+  if((buf[1] != 0) ||(buf[2] != 0))
+  {
+    USBD_HID_SendReport (&USB_OTG_dev, 
+                         buf,
+                         4);
+  } 
+}
+
+/**
+  * @brief  This function handles EXTI0_IRQ Handler.
+  * @param  None
+  * @retval None
+  */
+void EXTI0_IRQHandler(void)
+{
+  /* Clear the EXTI line pending bit */
+  EXTI_ClearITPendingBit(EXTI_Line0); // User button
+}
+
+/**
+  * @brief  This function handles EXTI15_10_IRQ Handler.
+  * @param  None
+  * @retval None
+  */
+void OTG_FS_WKUP_IRQHandler(void)
+{
+  if(USB_OTG_dev.cfg.low_power)
+  {
+        /* Reset SLEEPDEEP and SLEEPONEXIT bits */
+        SCB->SCR &= (uint32_t)~((uint32_t)(SCB_SCR_SLEEPDEEP_Msk | SCB_SCR_SLEEPONEXIT_Msk));
+
+        /* After wake-up from sleep mode, reconfigure the system clock */
+        SystemInit();
+    USB_OTG_UngateClock(&USB_OTG_dev);
+  }
+  EXTI_ClearITPendingBit(EXTI_Line18);
+}
+
+/**
+* @brief  This function handles OTG_HS Handler.
+* @param  None
+* @retval None
+*/
+void OTG_FS_IRQHandler(void)
+{
+  USBD_OTG_ISR_Handler (&USB_OTG_dev);
+}
+
+/**
+* @brief  USBD_HID_GetPos
+* @param  None
+* @retval Pointer to report
+*/
+static uint8_t *USBD_HID_GetPos (void)
+{
+  int8_t  x = 0 , y = 0 ;
+  static uint8_t HID_Buffer [4];
+  
+  x += CURSOR_STEP;
+  y += CURSOR_STEP;
+  
+  HID_Buffer[0] = 0;
+  HID_Buffer[1] = x;
+  HID_Buffer[2] = y;
+  HID_Buffer[3] = 0;
+  
+  return HID_Buffer;
+}
+
