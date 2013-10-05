b80e6d49f2f345a1b8cd8d4723b8b8695c950d7e
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2013-10-05 12:16:53 +0000

    Configuration

--- trunk/usb-vendor-specific/src/stm32fxxx_it.c
+++ trunk/usb-vendor-specific/src/stm32fxxx_it.c
@@ -1,50 +1,14 @@
-/**
-  ******************************************************************************
-  * @file    stm32fxxx_it.c
-  * @author  MCD Application Team
-  * @version V1.1.0
-  * @date    19-March-2012
-  * @brief   Main Interrupt Service Routines.
-  *          This file provides all exceptions handler and peripherals interrupt
-  *          service routine.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */ 
-
-/* Includes ------------------------------------------------------------------*/
 #include "stm32fxxx_it.h"
 #include "usb_core.h"
 #include "usbd_core.h"
 #include "usbd_hid_core.h"
 #include "usb_conf.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-
-#define CURSOR_STEP     10
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
 __IO uint32_t remote_wakeup =0;
 /* Private function prototypes -----------------------------------------------*/
 extern USB_OTG_CORE_HANDLE           USB_OTG_dev;
-static uint8_t *USBD_HID_GetPos (void);
 extern uint32_t USBD_OTG_ISR_Handler (USB_OTG_CORE_HANDLE *pdev);
+
 #ifdef USB_OTG_HS_DEDICATED_EP1_ENABLED 
 extern uint32_t USBD_OTG_EP1IN_ISR_Handler (USB_OTG_CORE_HANDLE *pdev);
 extern uint32_t USBD_OTG_EP1OUT_ISR_Handler (USB_OTG_CORE_HANDLE *pdev);
@@ -147,18 +111,11 @@ void PendSV_Handler(void)
 * @param  None
 * @retval None
 */
-void SysTick_Handler(void)
+void SysTick_Handler (void)
 {
-  
-  uint8_t *buf;
-  
-  buf = USBD_HID_GetPos();
-  if((buf[1] != 0) ||(buf[2] != 0))
-  {
-    USBD_HID_SendReport (&USB_OTG_dev, 
-                         buf,
-                         4);
-  } 
+
+        uint8_t buf[] = { 0x66, 0x77, 0x88, 0x99 };
+        vendorSendReport (&USB_OTG_dev, buf, 4);
 }
 
 /**
@@ -199,26 +156,5 @@ void OTG_FS_WKUP_IRQHandler(void)
 void OTG_FS_IRQHandler(void)
 {
   USBD_OTG_ISR_Handler (&USB_OTG_dev);
-}
-
-/**
-* @brief  USBD_HID_GetPos
-* @param  None
-* @retval Pointer to report
-*/
-static uint8_t *USBD_HID_GetPos (void)
-{
-  int8_t  x = 0 , y = 0 ;
-  static uint8_t HID_Buffer [4];
-  
-  x += CURSOR_STEP;
-  y += CURSOR_STEP;
-  
-  HID_Buffer[0] = 0;
-  HID_Buffer[1] = x;
-  HID_Buffer[2] = y;
-  HID_Buffer[3] = 0;
-  
-  return HID_Buffer;
 }
 
